efce43d5708411994619122d605982b8ff3d7e42
MohammadFakhrizaMaftukhin <fakhriza18b@gmail.com>
2025-04-23 23:20:34 +0700

    penerapan registrasi pengguna dengan validasi input

--- app/src/main/java/com/example/proyekutssmt4/RegisterActivity.kt
+++ app/src/main/java/com/example/proyekutssmt4/RegisterActivity.kt
@@ -1,20 +1,50 @@
 package com.example.proyekutssmt4
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.proyekutssmt4.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private val viewModel: UserViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+        binding.lifecycleOwner = this
+        binding.userViewModel = viewModel
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val name = binding.edtName.text.toString()
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+            val phone = binding.edtNoHp.text.toString()
+            val address = binding.edtAddress.text.toString()
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty() || phone.isEmpty() || address.isEmpty()) {
+                Toast.makeText(this, "Semua field harus diisi", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val user = User(
+                name = name,
+                email = email,
+                password = password,
+                phone = phone,
+                address = address
+            )
+            viewModel.register(user)
+            Toast.makeText(this, "Berhasil mendaftar", Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
