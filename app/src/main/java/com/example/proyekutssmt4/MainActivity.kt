e307efeb38a9a73f3a3382ab0b88086eef2113db
MohammadFakhrizaMaftukhin <fakhriza18b@gmail.com>
2025-04-23 23:27:04 +0700

    penerapan manajemen profil pengguna yang dilengkapi edit dan keluar

--- app/src/main/java/com/example/proyekutssmt4/MainActivity.kt
+++ app/src/main/java/com/example/proyekutssmt4/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.proyekutssmt4
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.proyekutssmt4.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: UserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[UserViewModel::class.java]
+        binding.lifecycleOwner = this
+
+        val email = intent.getStringExtra("email") ?: return
+
+        viewModel.observeUser(email).observe(this) { user: User ->
+            binding.user = user
+
+            binding.btnEdit.setOnClickListener {
+                val intent = Intent(this@MainActivity, UpdateProfileActivity::class.java)
+                intent.putExtra("email", email)
+                startActivity(intent)
+            }
+
+            binding.btnLogout.setOnClickListener {
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
